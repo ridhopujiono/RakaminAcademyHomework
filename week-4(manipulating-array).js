971c68656e94fa8d4c91ee8dd68b094ddf8a27b7
ridhopujiono <ridho@smktjp.sch.id>
2023-01-27 17:46:22 +0700

    Generate a random array

--- week-4(manipulating-array).js
+++ week-4(manipulating-array).js
@@ -5,4 +5,7 @@
  *  */
 function printArray(arr){
     return("[\n" + arr.join(", ") + "\n]");
- }
+}
+
+// Generate a random array
+let randArray = Array.from({length: 100}, () => Math.floor(Math.random() * 50) + 1);
