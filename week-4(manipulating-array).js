c7ff53d9baa1aa627af0635ff26ebf44c0567e5a
ridhopujiono <ridho@smktjp.sch.id>
2023-01-27 21:40:56 +0700

    try console all function

--- week-4(manipulating-array).js
+++ week-4(manipulating-array).js
@@ -21,7 +21,7 @@ function splitIndex(arr, type){
           if (i %2 == 0)  {
              result.push(arr[i]);
           }
-       } else if (type == "odd") {
+       } else {
           if (i %2 != 0) {
              result.push(arr[i]); 
           }
@@ -51,5 +51,29 @@ function findMaxMin(arr, type){
 // Generate a random array
 let randArray = Array.from({length: 100}, () => Math.floor(Math.random() * 50) + 1);
 // Get even and odd index
-let evenIndex = splitIndex(randArray, 'even');
-let oddIndex = splitIndex(randArray, 'odd');
+let evenIndex = splitIndex(randArray, "even");
+let oddIndex = splitIndex(randArray, "odd");
+// max
+let maxArrayEven = findMaxMin(evenIndex, 'max');
+let maxArrayOdd = findMaxMin(oddIndex, 'max');
+// min
+let minArrayEven = findMaxMin(randArray, 'min');
+let minArrayOdd = findMaxMin(randArray, 'min');
+
+console.log("============================================");
+console.info("SEMUA ARRAY");
+console.log(printArray(randArray));
+console.info("ARRAY DENGAN [INDEX] GENAP");
+console.log(printArray(evenIndex));
+console.info("ARRAY DENGAN [INDEX] GANJIL");
+console.log(printArray(oddIndex));
+console.log("============================================");
+console.info("MAKSIMUM ARRAY PADA [INDEX] ARRAY GENAP");
+console.log(maxArrayEven);
+console.info("MAKSIMUM ARRAY PADA [INDEX] ARRAY GANJIL");
+console.log(maxArrayOdd);
+console.info("MINIMUM ARRAY PADA [INDEX] ARRAY GENAP");
+console.log(minArrayEven);
+console.info("MINIMUM ARRAY PADA [INDEX] ARRAY GANJIL");
+console.log(minArrayOdd);
+console.log("============================================");
