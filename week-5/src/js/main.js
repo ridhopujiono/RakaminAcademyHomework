907f44811a16d22c292a3ed4f220d861a30b1bb5
Ridho Pujiono <ridho@smktjp.sch.id>
2023-02-04 22:26:39 +0700

    make method save array

--- week-5/src/js/main.js
+++ week-5/src/js/main.js
@@ -6,6 +6,13 @@ class Employee {
         this.age = age;
         this.pocket_money = pocket_money;
     }
+    saveToArray(name, age, pocket_money) {
+        this.data.push({
+            name: name,
+            age: parseInt(age),
+            pocket_money: parseInt(pocket_money),
+        })
+    }
     filterData(name, age, pocket_money) {
         let li;
         let element_ul = document.createElement("ul");
@@ -29,6 +36,78 @@ class Employee {
         }
 
         return true;
+    }
+    updateTable(name, age, pocket_money) {
+        let tbody = document.querySelector("#tbody-id");
+        let tr = document.createElement('tr');
+        let name_column = document.createElement('td');
+        name_column.innerHTML = name;
+        let age_column = document.createElement('td');
+        age_column.innerHTML = age;
+        let pocket_money_column = document.createElement('td');
+        pocket_money_column.innerHTML = pocket_money;
+
+        tr.appendChild(name_column);
+        tr.appendChild(age_column);
+        tr.appendChild(pocket_money_column);
+
+        tbody.appendChild(tr);
+
+        return true;
+    }
+    getTotalResumeData() {
+        let new_age = 0;
+        let new_pocket_money = 0;
+        for (let i = 0; i < this.data.length; i++) {
+            new_age += this.data[i].age;
+            new_pocket_money += this.data[i].pocket_money
+        }
+
+        return {
+            age: new_age,
+            pocket_money: new_pocket_money,
+        };
+    }
+    getAverageResume() {
+        let total_data = this.data.length;
+        let avg_age = Math.floor(this.getTotalResumeData().age / total_data);
+        let avg_pocket_money = Math.floor(this.getTotalResumeData().pocket_money / total_data);
+
+        return {
+            age: avg_age,
+            pocket_money: avg_pocket_money,
+        }
+    }
+    updateResume() {
+        let resume = document.querySelector("#resume");
+        let resume_content;
+        resume_content = `
+            <div class="row">
+                <div class="col-md-6">
+                <div class="card text-start">
+                    <div class="card-body">
+                    <h6 class="card-title">Rata - Rata Uang Sangu:</h6>
+                    <div class="card-body">
+                        <p>${this.getAverageResume().pocket_money}</p>
+                        <small class="text-danger">* pembulatan kebawah</small>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <div class="col-md-6">
+                <div class="card text-start">
+                    <div class="card-body">
+                    <h6 class="card-title">Rata - Rata Umur:</h6>
+                    <div class="card-body">
+                        <p>${this.getAverageResume().age}</p>
+                        <small class="text-danger">* pembulatan kebawah</small>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            `;
+        resume.innerHTML = resume_content
     }
     storeData(name, age, pocket_money) {
         // Call function for filter data
